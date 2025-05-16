fe97cc7dc93e8482be223f771a1cb284f712fe56
David Märzinger <a12325395@unet.univie.ac.at>
2025-05-16 12:59:20 +0200

    added 3 fragments for the screens

--- app/src/main/java/ac/at/univie/hci/learneasy/MainActivity.java
+++ app/src/main/java/ac/at/univie/hci/learneasy/MainActivity.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Fragment home = new HomeFragment();
+        Fragment profile = new ProfileFragment();
+        Fragment shop = new ShopFragment();
+
+        setFragment(home);
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
 }
